b288baa5534e238049600a92cee688cfaa834f8c
Holden <120606057+HCN1202@users.noreply.github.com>
2023-03-28 21:36:12 -0400

    final

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,54 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicleBaseClass")
 
+class Car extends VehicleModule{
+    constructor(make, model, year, color, mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.passenger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
+        this.started = false;
+        this.numberOfWheels = 0;
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            console.log("engine started");
+            return this.started = true;
+        } else {
+            console.log("engine cannot start!");
+            return this.started = false;
+        }
+    }
+
+    checkService(){
+        if (this.mileage > 30000)
+        this.scheduleService = true;
+        return this.scheduleService
+    }
+    loadPassanger(num){
+        if (this.maxPassenger > this.passanger){
+            if((num + this.passanger) <= this.maxPassenger){
+                this.passenger = num;
+                return this.passenger;
+            } else {
+                console.log(this.model + " " + this.make + " not enough space for all");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " full");
+        }
+    }
+}
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
